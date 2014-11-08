573982b75d56a3cc6aeabb34eb22c462900de303
Steve Pike <me@stevepike.co.uk>
2014-11-08 11:06:28 +0000

    finish first pass of schema

--- maps/models.py
+++ maps/models.py
@@ -133,6 +133,58 @@ class Event(models.Model):
     )
 
 
+class DataSource(models.Model):
+    """Satellite name and sensor type."""
+    source_type = models.CharField(
+        choices=(
+            ('SATELLITE', 'Satellite data'),
+            ('ADMIN', 'Admin boundaries'),
+            ('ROADS', 'Roads'),
+            ('HYDRO', 'Hydrography'),
+            ('ELEVATION', 'Elevation'),
+            ('SETTLEMENTS', 'Settlements'),
+            ('HEALTH', 'Health facilities'),
+            ('SHCOOLS', 'Schools'),
+            ('SHELTER', 'Shelter'),
+            ('POPULATION', 'Population'),
+            ('IMPACT', 'Impact indicators/statistics'),
+            ('NEEDS', 'Needs'),
+            ('RESOURCING', 'Resourcing'),
+            ('GENERAL', 'General')
+        ), max_length=20,
+    )
+    name = models.CharField(max_length=255)
+    meta = models.DictionaryField()
+
+    def __unicode__(self):
+        return "{0} - {1}".format(self.name, self.sensor_type)
+
+
+class StatisticalOrIndicatorData(models.Model):
+    data_type = models.CharField(
+        max_length=255,
+        null=True, blank=True,
+    )
+    is_pre_or_post = models.ChoiceField(
+        choices=(
+            ('PRE', 'Pre-event'),
+            ('POST', 'Post-event'),
+        ),
+        max_length=50,
+        null=True, blank=True,
+    )
+    data_date_earliest = models.DateField(
+        null=True, blank=True,
+    )
+    data_date_latest = models.DateField(
+        null=True, blank=True,
+    )
+    data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+
 class Map(models.Model):
     """Storage of a Map object."""
     reviewer_name = models.CharField(max_length=300)
@@ -203,3 +255,215 @@ class Map(models.Model):
         help_text="Infographics or other non-map items in map.",
         null=True, blank=True
     )
+    disclaimer = models.TextField(
+        null=True, blank=True
+    )
+    copyright = models.TextField(
+        null=True, blank=True
+    )
+
+    # Satellite data group
+    has_satellite_data = models.BooleanField()
+    phase_type = models.ChoiceField(
+        help_text="Is it pre or post disaster imagery?",
+        max_length=255, choices=(),
+        null=True, blank=True,
+    )
+    satellite_data_date = models.DateField(null=True, blank=True)
+    satellite_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True
+    )
+
+    # Admin boundaries group
+    has_admin_boundaries = models.BooleanField()
+    admin_max_detail_level = models.CharField(
+        choices=(), max_length=50,
+        null=True, blank=True,
+    )
+    admin_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+    # Roads group
+    has_roads = models.BooleanField()
+    roads_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+    # Hydrography group
+    has_hydrographic_network = models.BooleanField()
+    hydrographic_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+    # Elevation group
+    has_elevation_data = models.BooleanField()
+    elevation_data_type = models.CharField(
+        choices=(), max_length=50,
+        null=True, blank=True,
+    )
+    elevation_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+    # Settlements group
+    has_settlements_data = models.BooleanField()
+    settlements_max_detail_level = models.CharField(
+        choices=(), max_length=50,
+        null=True, blank=True,
+    )
+    settlements_data_type = models.CharField(
+        choices=(), max_length=50,
+        null=True, blank=True,
+    )
+    settlements_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+    # Health data
+    has_health_data = models.BooleanField()
+    health_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+    # Schools group
+    has_schools_data = models.BooleanField()
+    schools_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+
+    # Shelter group
+    has_shelter_data = models.BooleanField()
+    shelter_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+    shelter_data_date = models.DateField(
+        null=True, blank=True,
+    )
+
+    # Physical impact
+    has_impact_geographic_extent = models.BooleanField()
+    #    TODO
+#    impact_data_types = models.ManyToMany(
+#        ImpactDataType
+#    )
+    impact_data_source_type = models.CharField(
+        choices=(
+            ('MODEL', 'Modelled/predicted'),
+            ('OBSERVATION', 'Observed'),
+        ), max_length=20,
+        null=True, blank=True,
+    )
+    impact_situational_date_earliest = models.DateField(
+        null=True, blank=True,
+    )
+    impact_situational_date_latest = models.DateField(
+        null=True, blank=True,
+    )
+    #    TODO:
+#    damaged_objects = models.ManyToMany(
+#        ObjectType
+#    )
+    damage_situational_date_earliest = models.DateField(
+        null=True, blank=True,
+    )
+    damage_situational_date_latest = models.DateField(
+        null=True, blank=True,
+    )
+
+    # Population group
+    has_population_data = models.BooleanField()
+    population_data_type = models.ChoiceField(
+        choices=(),
+        max_length=50,
+        null=True, blank=True,
+    )
+    population_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+    population_data_date_earliest = models.DateField(
+        null=True, blank=True,
+    )
+    population_data_date_latest = models.DateField(
+        null=True, blank=True,
+    )
+
+    has_affected_population_data = models.BooleanField()
+    # TODO:
+#    humanitarian_profile_level_1_types = models.ManyToMany(
+#        HumanitarianProfileLevelType
+#    )
+#    disaggregated_affected_population_types = models.ManyToMany(
+#        DisaggregatedAffectedPopulationType
+#    )
+
+    affected_population_data_date_earliest = models.DateField(
+        null=True, blank=True,
+    )
+    affected_population_data_date_latest = models.DateField(
+        null=True, blank=True,
+    )
+    affected_population_data_source = models.ManyToMany(
+        DataSource,
+    )
+
+    # Statistical data
+    has_statistical_data = models.BooleanField()
+    statistical_data = models.ManyToMany(
+        StatisticalOrIndicatorData
+    )
+
+    # Needs, activites & gaps
+    # TODO:
+#    active_clusters = models.ManyToMany(
+#        Cluster
+#    )
+    has_subcluster_information = models.BooleanField()
+    has_activity_detail = models.BooleanField()
+    # TODO:
+#    assessments = models.ManyToMany(
+#        Assessment
+#    )
+    has_humanitarian_needs = models.BooleanField()
+    humanitarian_needs_data_date_earliest = models.DateField(
+        null=True, blank=True,
+    )
+    humanitarian_needs_data_date_latest = models.DateField(
+        null=True, blank=True,
+    )
+    humanitarian_needs_data_source = models.ForeignKey(
+        DataSource,
+        null=True, blank=True,
+    )
+    # FIXME
+    gaps = models.MultipleChoiceField(
+        choices=(),
+        max_length=50,
+    )
+    resourcing_or_funding = models.MultipleChoiceField(
+        choices=(),
+        max_length=50,
+    )
+    resourcing_data_date_earliest = models.DateField(
+        null=True, blank=True,
+    )
+    resourcing_data_date_latest = models.DateField(
+        null=True, blank=True,
+    )
+
+    # Other
+#    # TODO
+#    additional_datasets = models.ManyToMany(
+#        AdditionalDataset
+#    )
+    indirect_datasets = models.TextField(blank=True, null=True)
